7bfd01ebcef6100118886ae1a3175f8239c7bcdf
Furkan Toy <furkantoy@hotmail.com>
2025-11-18 00:15:13 +0300

    ilkdeneme

---
+++ src/controllers/contacts.js
@@ -0,0 +1,23 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res, next) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactsByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
